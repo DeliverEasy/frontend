11f388ae4c5ca0058baf074d840d1d55db22590a
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-04 09:54:52 -0300

    Minor changes

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -6,42 +6,42 @@ import './styles.scss';
 
 class LoginForm extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    handle_change = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+        console.log(name + ' ' + value)
+        this.setState(prevstate => {
+            const newState = {
+                ...prevstate
+            };
+            newState[name] = value;
+            return newState;
+        });
     };
-  }
 
-  handle_change = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-    console.log(name + ' ' + value)
-    this.setState(prevstate => {
-      const newState = {
-        ...prevstate
-      };
-      newState[name] = value;
-      return newState;
-    });
-  };
+    handle_login = (e, creds) => {
+        this.props.loginUser(creds);
+    }
 
-  handle_login = (e, creds) => {
-    this.props.loginUser(creds);
-  }
+    render() {
+        return (
+            <div>
+                <form>
+                    <input name="username" type="text" placeholder="Email" value={this.state.username} onChange={this.handle_change} />
+                    <input name="password" type="password" placeholder="Password" onChange={this.handle_change} value={this.state.password} />
+                    <button type="button" onClick={e => this.handle_login(e, this.state)}>Login</button>
+                </form>
 
-  render() {
-    return (
-      <div>
-        <form>
-          <input name="username" type="text" placeholder="Email" value={this.state.username} onChange={this.handle_change} />
-          <input name="password" type="password" placeholder="Password" onChange={this.handle_change} value={this.state.password} />
-          <button type="button" onClick={e => this.handle_login(e, this.state)}>Login</button>
-        </form>
-
-      </div>);
-  }
+            </div>);
+    }
 }
 
 export default connect(null, { loginUser })(LoginForm);
