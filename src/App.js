b61887e92d15224457259aafc44071da57ca6302
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-04 09:55:44 -0300

    Added token verification on componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { verifyToken } from './actions';
 import NavBar from './components/NavBar';
 
 class App extends Component {
 
-  render() {
-    return (
-      <div>
-        <NavBar />
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.props.verifyToken(localStorage.token)
+    }
+
+    render() {
+        return (
+            <div>
+                <NavBar />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default connect(null, {verifyToken})(App);
