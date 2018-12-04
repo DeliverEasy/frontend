a2a2d40ee5b9318d05cf0b642ba9e94ff663ca34
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-04 09:55:09 -0300

    Added logout button to NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions';
 import './styles.scss';
 import LoginForm from '../LoginForm';
 
 class NavBar extends Component {
-  render() {
-    if (this.props.isAuthenticated) {
-      return (<div>{localStorage.token}</div>);
-    } else {
-      return (<div><LoginForm /></div>);
+
+    render() {
+        if (this.props.isAuthenticated) {
+            return (
+                <div>
+                    {localStorage.token}
+                    <button onClick={e => this.props.logoutUser()}>Logout</button>
+                </div>);
+        } else {
+            return (<div><LoginForm /></div>);
+        }
     }
-  }
 }
 
 function mapStateToProps(state) {
-  console.log(state.isAuthenticated);
-  return { isAuthenticated: state.isAuthenticated }
+    console.log(state.isAuthenticated);
+    return { isAuthenticated: state.isAuthenticated }
 }
 
-export default connect(mapStateToProps)(NavBar);
+export default connect(mapStateToProps, { logoutUser })(NavBar);
