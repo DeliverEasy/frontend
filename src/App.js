0b0eb6b24c91838c83d01e3a10d23b0fea8e7ccb
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-04 20:59:51 -0300

    Various changes in render method related with authentication

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { verifyToken } from './actions';
+import './styles.scss';
+
 import NavBar from './components/NavBar';
+import SearchPosts from './scenes/SearchPosts';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
     componentDidMount() {
-        this.props.verifyToken(localStorage.token)
+        this.props.verifyToken(localStorage.token);
     }
 
     render() {
-        return (
-            <div>
-                <NavBar />
-            </div>
-        );
+        if (this.props.isAuthenticated) {
+            return (
+                <div>
+                    <NavBar />
+                    <div className="scene-container">
+                        <SearchPosts />
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <NavBar />
+                    <div className="scene-container">
+                        <LoginForm />
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
-export default connect(null, {verifyToken})(App);
+function mapStateToProps(state) {
+    return { isAuthenticated: state.isAuthenticated }
+}
+
+export default connect(mapStateToProps, { verifyToken })(App);
