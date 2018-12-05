7e17f39e1e66f5b4529564c392654553bff32c72
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-04 21:04:10 -0300

    Added SearchBar dummy Component

---
+++ src/scenes/SearchPosts/components/SearchBar/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <input className='search_bar'></input>
+        );
+    }
+}
+
+export default SearchBar;
