e326b6e2474dc98c7e61d1941d7b8a2eeed3178e
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-04 21:03:55 -0300

    Added PostDisplayer dummy Component

---
+++ src/scenes/SearchPosts/components/PostDisplayer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class PostDisplayer extends Component {
+
+    render() {
+        return (
+            <div className='post-displayer'>POST EXAMPLE</div>
+        );
+    }
+}
+
+export default PostDisplayer;
