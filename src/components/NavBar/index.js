6151ff9c409ebbf7e2cb40c9703788a9493c748a
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-06 19:40:28 -0300

    Hardcoded main buttons to NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,10 +9,16 @@ class NavBar extends Component {
         if (this.props.isAuthenticated) {
             return (
                 <div className='nav-bar'>
-                    <button onClick={e => this.props.logoutUser()}>LO</button>
+                    <div className='nav-bar-navigation-button-container'>
+                        <button className='nav-bar-button active'><i className="icon material-icons">search</i></button>
+                        <button className='nav-bar-button'><i className="icon material-icons">home</i></button>
+                    </div>
+                    <div className='nav-bar-account-button-container'>
+                        <button className='nav-bar-button' onClick={e => this.props.logoutUser()}><i className="icon material-icons">power_settings_new</i></button>
+                    </div>
                 </div>);
         } else {
-            return (<div className='nav-bar'>Not Authenticated</div>);
+            return (<div className='nav-bar'></div>);
         }
     }
 }
